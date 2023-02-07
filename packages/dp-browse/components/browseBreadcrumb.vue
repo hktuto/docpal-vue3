81cfaed45dbe3cd159ab277968bfc4062c0e292c
Sean Tsang <seantsang83@gmail.com>
2023-02-07 18:09:17 +0800

    breadcrumb setup

--- packages/dp-browse/components/browseBreadcrumb.vue
+++ packages/dp-browse/components/browseBreadcrumb.vue
@@ -1,38 +1,73 @@
 <template>
     <div class="breadCrumbContainer">
         <!-- {{data}} -->
-        <div v-if="data && data.length > 0" class="upLevel">
-            <ElButton type="info" :icon="Back" circle @click="backToParent"/>
-        </div>
         <div v-loading="pending" class="breadContainer">
-            <div v-for="item in data" :key="data.id" class="breadItem">
-                {{item.name}}
+            <div class="breadItem pointer" @click="navigate(rootPath)">
+                Home
             </div>
+            <div v-if="data.length > 0" class="divider">
+                <ElIcon><ArrowRight/></ElIcon>
+            </div>
+            <template v-for="(item,index) in data" :key="data.id">
+                <div  :class="{breadItem:true, pointer: index < data.length - 1 }" @click="navigate(item.path)">
+                    {{item.name}}
+                </div>
+                <div v-if="index < data.length - 1" class="divider">
+                    <ElIcon><ArrowRight/></ElIcon>
+                </div>
+            </template>
+
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import {Back} from '@element-plus/icons-vue'
+import {Back, ArrowRight} from '@element-plus/icons-vue'
 const router = useRouter();
-const props = defineProps<{
-    path:string
-}>()
+const props = withDefaults(defineProps<{
+    path:string,
+    rootPath: string
+}>(),{
+    path:'/',
+    rootPath: '/'
+})
 
 const { data, refresh, pending } = useAsyncData(() => getBreadcrumb(props.path));
 watch(props, () => refresh())
 
-async function backToParent() {
-    router.push()
+function navigate(path) {
+    router.push({
+        path:'/browse',
+        query:{
+            path: path
+        }
+    })
 }
 </script>
 
 <style lang="scss" scoped>
+.breadCrumbContainer{
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap : calc(var(--el-component-size-small) / 3 );
+}
 .breadContainer{
     display: flex;
-    gap: var(--el-component-size-small);
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    align-items: center;
+    gap: calc(var(--el-component-size-small) / 3 );
 }
 .breadItem{
-
+    padding: 6px;
+    &.pointer{
+        border-radius: 4px;
+        &:hover{
+            background: var(--el-color-primary);
+            color: var(--el-color-white);
+        }
+    }
 }
 </style>
