333bbd3bfb98d9de5b50a933909a70c2b2b59106
Sean Tsang <seantsang83@gmail.com>
2023-02-07 13:46:36 +0800

    add api

--- packages/dp-stores/utils/documentApi.ts
+++ packages/dp-stores/utils/documentApi.ts
@@ -12,3 +12,28 @@ export const GetChildThumbnail = async(idOrPath: string):Promise<GetChildRespons
         body:{ idOrPath },
     })
 }
+
+export type DocDetail = {
+
+}
+export const GetDocDetail = async(id:string) => {
+    return api<GetChildResponse>('/nuxeo/document', {
+        method:'POST' ,
+        params:{ id },
+    })
+}
+
+export const getBreadcrumb = async(idOrPath:string) => {
+    console.log("getBreadcrumb");
+    return api<GetChildResponse>('/nuxeo/document/breadcrumb', {
+        method:'POST' ,
+        body:{ idOrPath },
+    })
+}
+
+export const getChild = async(idOrPath:string) => {
+    return api<GetChildResponse>('/nuxeo/document/children', {
+        method:'POST' ,
+        body:{ idOrPath },
+    })
+}
