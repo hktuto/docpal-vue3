6bc0c07444dcc5c261b4ab0287272c20633dbbcd
Oy <OY.Ou@weltronics.com>
2023-03-02 16:19:01 +0800

    column

---
+++ packages/dp-components/stores/index.ts
@@ -0,0 +1,140 @@
+import dayjs from 'dayjs'
+export type metaType = 'string' | 'date' | 'boolean' | 'number'
+export const trashType = {
+  id: 'string',
+  name: 'string',
+  description: 'string',
+  path: 'string',
+  type: 'string',
+  version: 'string',
+  createdBy: 'string',
+  createdDate: 'date',
+  modifiedDate: 'date',
+  isFolder: 'boolean',
+  isCheckedOut: 'boolean',
+  properties: {
+    "uid:uid": 'string',
+    "uid:major_version": 'number',
+    "uid:minor_version": 'number',
+    "trashed_date": 'date',
+    "principalName": 'string'
+  },
+  parentRef: 'string',
+  logicalPath: 'string'
+}
+export function propListGet (typeObj: any) {
+  const result:any = []
+  recursion(typeObj)
+  return result
+  function recursion(obj:any, prefix = '') {
+    Object.keys(obj).forEach(key => {
+      if (typeof obj[key] === 'object') {
+        prefix += key + '.'
+        recursion(obj[key], prefix)
+      }
+      else result.push({value: prefix + key, label: prefix + key})
+    })
+  }
+}
+export function getPropType(row: any, prop: string) {
+  let result = ''
+  let nextValue = { ...row }
+  prop.split('.').forEach(key => {
+      result = nextValue[key]
+      nextValue = nextValue[key]
+  })
+  return result
+}
+export function getFilterArr (metaType: metaType) {
+  const result:any = []
+  filterArr.forEach(item => {
+    if(!item.supportedType || item.supportedType.includes(metaType)) {
+      result.push({ lable: item.name, value: item.name, ...item })
+    }
+  })
+  return result
+}
+export const filterArr = [
+  {
+    name: 'dateFormat',
+    supportedType: ['date'],
+    params: {
+      format: 'string'
+    }
+  }, {
+    name: 'capitalized',
+    supportedType: ['string'],
+  }, {
+    name: 'hidePhone',
+    supportedType: ['number'],
+  }, {
+    name: 'hideIdCard',
+    supportedType: ['number'],
+  },{
+    name: 'hideEmail',
+    supportedType: ['string'],
+  },{
+    name: 'regExp',
+    params: {
+      regExp: 'string',
+      format: 'string'
+    },
+    example: [
+      {
+        regExp: '/^(\\d{6})\\d+(\\d{4})$/',
+        format: '$1******$2',
+        description: 'regExp.description.hideIdCard'
+      }
+    ]
+  }
+]
+
+
+const ValueFilters = {
+    dateFormat(time:string = '', params:any){
+      console.log({time},params);
+      
+        if(!time) return ''
+        if(!params.format) params.format = 'YYYY-MM-DD HH:mm'
+        return dayjs(time).format( params.format)
+    },
+    capitalized(str: string){
+        str = str.toLowerCase();
+        return str.replace(/\b(\w)|\s(\w)/g, (m) => m.toUpperCase())
+    },
+    hideEmail (email: string) {
+        if (String (email).indexOf ('@') > 0) {
+            let newEmail, str = email.split('@'), _s = '';
+    
+            if (str[0].length > 4) {
+                _s = str[0].substr (0, 4);
+                for (let i = 0; i < str[0].length - 4; i++) {
+                    _s += '*';
+                }
+            } else {
+                _s = str[0].substr (0, 1);
+                for (let i = 0; i < str[0].length - 1; i++) {
+                    _s += '*';
+                }
+            }
+            newEmail = _s + '@' + str[1];
+            return newEmail;
+        } else {
+            return email;
+        }
+    },
+    /* 数字 */
+    hidePhone(value: any) {
+        if(!Number(value)) return Number(value)
+        return value.replace(/^(\d{3})\d+(\d{4})$/, "$1****$2");
+    },
+    hideIdCard(value: any) {
+        if(!Number(value)) return Number(value)
+        return value.replace(/^(\d{6})\d+(\d{4})$/, "$1******$2");
+    },
+    regExp (value: any, params: any) {
+        return value.replace(eval(params.regExp), params.format)
+    }
+}
+
+export default ValueFilters
