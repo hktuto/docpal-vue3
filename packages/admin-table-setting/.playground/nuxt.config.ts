6e98651cbb64c54e8965f53b43580524b5321dfc
Oy <OY.Ou@weltronics.com>
2023-03-02 17:43:43 +0800

    tableSetting draggable

--- packages/admin-table-setting/.playground/nuxt.config.ts
+++ packages/admin-table-setting/.playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 import playgroundConfig from '../../../utils/playgroundConfig'
 
 const config = defineNuxtConfig({
-  extends: ['..', '../../dp-stores', '../../layout-default', '../../dp-auth', '../../dp-form'],
+  extends: ['..', '../../dp-stores', '../../layout-default', '../../dp-auth', '../../dp-form', '../../dp-components'],
   modules: [
     '@nuxt/devtools',
   ],
