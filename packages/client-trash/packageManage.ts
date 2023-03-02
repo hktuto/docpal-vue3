75159508a0e811e427ef0f409d48f2d41b3a7a12
Sean Tsang <seantsang83@gmail.com>
2023-03-02 17:07:18 +0800

    fix build

--- packages/client-trash/packageManage.ts
+++
@@ -1,8 +0,0 @@
-
-
-export function customLayer():string[] {
-    const packages:string[] = [];
-    // all stores
-    packages.push("../../packages/dp-stores")
-    return packages;
-}
--- packages/dp-auth/app.config.ts
+++ packages/dp-auth/app.config.ts
@@ -1,13 +1,5 @@
 /// <reference path="../../global.d.ts" />
 
 export default defineAppConfig({
-  menu:[
-    {
-      name:'admin_setting',
-      routeName:"setting",
-      icon: '/icons/menu/user.svg',
-      url: '/setting',
-      order:99
-    }
-  ]
+  
 })
