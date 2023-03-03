fb4c1b910f824268dc925a9ef7402fcf17738c94
Sean Tsang <seantsang83@gmail.com>
2023-03-03 18:09:29 +0800

    tem

---
+++ packages/dp-log/nuxt.config.ts
@@ -0,0 +1,13 @@
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules:[
+        '@nuxtjs/color-mode',
+    ],
+    
+    colorMode:{
+        classSuffix:'',
+      },
+   
+    
+})
