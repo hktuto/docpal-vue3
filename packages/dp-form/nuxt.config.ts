76391c4bf60c5e99f9a6904cae3caf17dde010ce
Sean Tsang <seantsang83@gmail.com>
2023-03-06 13:09:53 +0800

    add form setting pagebase setup

--- packages/dp-form/nuxt.config.ts
+++ packages/dp-form/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
-    
+    modules: [
+        '@nuxt/content'
+      ],
+      content:{
+        api: {
+          baseURL:'/myEndPoint/_content'
+        }
+      }
 })
---
+++ packages/dp-form/pages/FormEditor/[id].vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="pageContainer">
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
