e8e7d8ac76c6a61b81efacad8d9160bd0f7364f9
Sean Tsang <seantsang83@gmail.com>
2023-03-10 14:14:46 +0800

    fix icon

--- packages/client-browse/pages/browse/index.vue
+++ packages/client-browse/pages/browse/index.vue
@@ -5,6 +5,7 @@
             <BrowseBreadcrumb ref="breadCrumb" :path="routePath" rootPath="/" />
             <BrowseTable :path="routePath" :doc="data"/>
         </div>
+        <SvgIconSelector v-model:src="icon" />
         <template v-if="data && !data.isFolder">
             <BrowseDetail :show="!data.isFolder" :doc="data" @close="detailClosed" />
         </template>
@@ -16,7 +17,9 @@
 <script lang="ts" setup>
 import { GetDocDetail } from 'dp-api'
 const breadCrumb = ref();
+const icon = ref("");
 const route = useRoute();
+
 const routePath = computed( () => route.query.path || '/')
 const { data, refresh, pending} = await useAsyncData(() => GetDocDetail(route.query.path));
 watch(route, () => refresh());
