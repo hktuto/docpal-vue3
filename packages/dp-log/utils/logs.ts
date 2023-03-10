a86b393a4318e88d37b1991d6d64b906afc9083f
Sean Tsang <seantsang83@gmail.com>
2023-03-10 10:39:39 +0800

    update log to store user session

--- packages/dp-log/utils/logs.ts
+++ packages/dp-log/utils/logs.ts
@@ -1,6 +1,12 @@
+
+
+
 export const dplog = (...args:any) => {
     const {dev} = useRuntimeConfig()
     if(dev){
         console.log(...args)
     }
+    // @ts-ignore
+    const userLog = useLog();
+    userLog.setUserLog(...args);
 }
