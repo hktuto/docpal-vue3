cb133b02e5a772e687a389d63aa41013f467bc49
Sean Tsang <seantsang83@gmail.com>
2023-03-14 09:03:06 +0800

    bg animate

--- packages/layout-default/components/LoadingBg/index.vue
+++ packages/layout-default/components/LoadingBg/index.vue
@@ -1,27 +1,34 @@
 <template>
     <div class="fullPage">
-        <canvas id="gradient-canvas"  data-transition-in/>
+        <canvas id="gradient-canvas" :class="{canvasInit}"  data-transition-in/>
     </div>
 </template>
 
 <script lang="ts" setup >
 import {Gradient} from './Gradient'
+import { useEventListener } from '@vueuse/core'
 const color1= '#ef008f';
 const color2= '#6ec3f4';
 const color3= '#7038ff';
 const color4= '#e2e2e2';
 
-
+const canvasInit = ref(false)
 
 const canvas = ref();
 
+function canvasRender(){
+    canvasInit.value = true;
+}
 function createGradient() {
+    canvasInit.value = false;
    const gradient = new Gradient()
     // Call `initGradient` with the selector to your canvas
     gradient.initGradient('#gradient-canvas');
 }
 
+
 onMounted(() => {
+    useEventListener(window,'miniGLRender', canvasRender);
     createGradient()
 })
 </script>
@@ -42,7 +49,12 @@ onMounted(() => {
         top:0;
         height: 100vh;
         width: 100%;
-        background: lightgrey;
+        background: var(--primary-color);
+        opacity: 0;
+        transition: opacity .3s ease-in-out;
+        &.canvasInit{
+            opacity: 1;
+        }
     }
 }
 </style>
