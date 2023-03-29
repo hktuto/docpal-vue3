c88fbc729ebe3ffe3938d4d97b289d9b016f9179
Oy <OY.Ou@weltronics.com>
2023-03-29 11:40:17 +0800

    public upload

---
+++ packages/public-upload/.playground/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="page"></div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ packages/public-upload/pages/public/upload.vue
@@ -0,0 +1,63 @@
+<template>
+<LoadingBg />
+<div class="app"  v-loading="state.loading">
+    <UploadForm v-if="uploadState" :fileRequestDetail="fileRequestDetail" @submit="handleWorkflow"></UploadForm>
+    <UploadPassword v-else @submit="handleGetPublicDocument" ></UploadPassword>
+</div>
+
+</template>
+
+
+<script lang="ts" setup>
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { publicFileRequestGetApi, publicFileRequestSaveApi, TABLE, defaultTableSetting } from 'dp-api'
+const route = useRoute()
+const router = useRouter()
+const state = reactive({
+    uploadState: '' ,
+    loading: false,
+    fileRequestDetail: {},
+    password: ''
+})
+const { uploadState, fileRequestDetail } = toRefs(state)
+async function handleGetPublicDocument (formData) {
+    state.password = formData.password
+    try {
+        state.loading = true
+        formData.token = route.query.token
+        state.fileRequestDetail = await publicFileRequestGetApi(formData)
+        state.fileRequestDetail.config = getFormData(state.fileRequestDetail.properties)
+        state.uploadState = true
+    } catch (error) {
+        router.push('/public/uploadTip?tip=linkExpired')
+    }
+    setTimeout(() => { state.loading = false }, 100)
+}
+function getFormData (properties) {
+    const result = <any>{}
+    properties.forEach(item => {
+        result[item.id] = item.value
+    })
+    return result
+}
+
+async function handleWorkflow(fileList: any) {
+    state.loading = true
+    const formData = new FormData()
+    formData.append('taskId', state.fileRequestDetail.task.id)
+    formData.append('password', state.password)
+    formData.append('token', route.query.token)
+    fileList.forEach((file) => {
+        formData.append('files', file.raw)
+    })
+    const res = await publicFileRequestSaveApi(formData)
+    if (res) router.push('/public/uploadTip?tip=uploadedSuccessfully')
+    setTimeout(() => { state.loading = false }, 100)
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+    height: 100vh;
+}
+</style>
