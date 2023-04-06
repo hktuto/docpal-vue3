9ff3054f3b84f0b2e14fc7eb214a86ef5855c2aa
Sean Tsang <seantsang83@gmail.com>
2023-04-06 13:49:16 +0800

    no ssr on system-form

--- packages/system-form/nuxt.config.ts
+++ packages/system-form/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
 export default defineNuxtConfig({
+  ssr:false,
   // extends:['../layout-default', '../admin-table-setting'],
   //   modules: [
   //     ],
