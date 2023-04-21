2c72d646cb3a1cea8a4c20d2b41b785f585825b8
Sean Tsang <seantsang83@gmail.com>
2023-04-21 11:31:57 +0800

    sync all version

--- packages/client-browse/.playground/nuxt.config.ts
+++ packages/client-browse/.playground/nuxt.config.ts
@@ -2,10 +2,8 @@ import playgroundConfig from '../../../utils/playgroundConfig'
 
 // @ts-ignore
 const config = defineNuxtConfig({
+  ssr: false,
   extends: ['..', '../../dp-stores', '../../dp-auth', '../../layout-default', '../../client-workflow' ],
-  modules: [
-    '@nuxt/devtools',
-  ],
   ...playgroundConfig({useFormRender:true}),
 })
 
