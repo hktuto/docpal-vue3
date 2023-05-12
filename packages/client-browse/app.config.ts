cb854ca64eab4ab4dff8ad0a4fd3beda0db226b5
Oy <OY.Ou@weltronics.com>
2023-05-12 17:47:31 +0800

    fix

--- packages/client-browse/app.config.ts
+++ packages/client-browse/app.config.ts
@@ -1,15 +1,7 @@
 /// <reference path="../../global.d.ts" />
 
 export default defineAppConfig({
-  menu:[
-    // {
-    //   name:"file_browse",
-    //   routeName:'browse',
-    //   url:"/browse",
-    //   icon: "/icons/menu/browser.svg",
-    //   order:0,
-    // }
-  ],
+
 })
 
 
