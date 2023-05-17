d6cbc4eb6c01d7935677c7fa593b93fd0fd3d32f
Sean Tsang <seantsang83@gmail.com>
2023-05-17 22:46:49 +0800

    remove not use component, fix tags api endpoint

--- packages/client-browse/components/browse/info/Tag/index.vue
+++ packages/client-browse/components/browse/info/Tag/index.vue
@@ -1,16 +1,6 @@
 <template>
    <div class="infoSection" v-show="tags.length > 0 || doc.canWrite">
      <div class="infoTitle">{{ $t('rightDetail_tags') }}</div>
-<!--    <div class="tagsTitle">-->
-<!--      <span class="title">{{ $t('rightDetail_tags') }}</span>-->
-<!--      <div class="icon" v-show="doc.canWrite">-->
-<!--        <BrowseInfoTagAdd-->
-<!--          :doc="doc"-->
-<!--          @handleAdd="handleDocumentTagsAdd"-->
-<!--        ></BrowseInfoTagAdd>-->
-<!--        &lt;!&ndash; <img class="cursorPointer" src="/icons/fold.svg" /> &ndash;&gt;-->
-<!--      </div>-->
-<!--    </div>-->
     <div class="infoContent">
       <VueTagsInput
           element-id="tags"
@@ -20,20 +10,17 @@
           :placeholder="$t('tip.input')"
           :typeahead="true"
           :disabled="!doc.canWrite"
-          @tags-updated="handleDocumentTagsChanged"
+          @tag-removed="handleTagsRemoved"
+          @tag-added="handleTagsAdded"
         />
-<!--      <el-tag  v-for="(tag, i) in tags" :key="i" effect="dark"-->
-<!--            :closable="doc.canWrite"-->
-<!--            @close="handleDocumentTagsDelete(tag)">-->
-<!--            {{ tag.label }}-->
-<!--      </el-tag>-->
+
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import '@james090500/vue-tagsinput/dist/style.css';
-import {PatchTags, SearchTagByName} from 'dp-api'
+import {DocumentAddTags, PatchTags, SearchTagByName} from 'dp-api'
 import { ElMessage } from 'element-plus'
 const props = defineProps<{
   doc: any
@@ -44,12 +31,18 @@ const { t} = useI18n()
 // TODO : add api
 // const { patchTags } = UseTags()
 
-
-async function handleDocumentTagsChanged(e:any){
+async function handleTagsAdded(tagSelected:any) {
+  const param = {
+        documentIdOrPath: props.doc.id,
+        labels: [tagSelected.value],
+      }
+  await DocumentAddTags(param)
+}
+async function handleTagsRemoved(e:any){
   console.log('handleDocumentTagsChange',tags.value)
   const param = {
         documentIdOrPath: props.doc.id,
-        labels: tags.value.map((item:any) => item.label),
+        labels: tags.value.map((item:any) => item.value),
       }
   await PatchTags(param)
   ElMessage.success(t('msg_successfullyModified') as string)
