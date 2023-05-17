a60e487f1b1a3ba4db0450b44731c5aff4ed3287
Sean Tsang <seantsang83@gmail.com>
2023-05-17 18:07:41 +0800

    add folder and file default icon

--- packages/client-browse/components/browse/ItemIcon.vue
+++ packages/client-browse/components/browse/ItemIcon.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="iconContainer">
-         <img :src="`/icons/${type}-${status}.svg`" />
+         <img :src="`icons/${type}-${status}.svg`" />
     </div>
 </template>
 
 <script lang="ts" setup>
 type IconType = 'folder' | 'file'
-type IconStatus = 'normal' | 'selected' 
+type IconStatus = 'normal' | 'selected'
  const props = withDefaults( defineProps<{
         type: IconType;
         status?: IconStatus;
