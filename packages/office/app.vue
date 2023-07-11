176298d5a4f4b5b210f7d730baa4ecfb19e68a2f
Sean Tsang <seantsang83@gmail.com>
2023-07-11 17:41:13 +0800

    try move office script to client app

--- packages/office/app.vue
+++ packages/office/app.vue
@@ -2,11 +2,5 @@
   <AuthApp :showForgetPassword="false"/>
 </template>
 <script setup lang="ts">
-useHead({
-  script:[
-    {
-      src:"https://appsforoffice.microsoft.com/lib/1/hosted/office.js",
-    }
-  ]
-})
+
 </script>
