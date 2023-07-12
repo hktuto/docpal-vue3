2f54ad5b2f03923212b367073f6f02ad87c7f99d
Sean Tsang <seantsang83@gmail.com>
2023-07-12 18:13:33 +0800

    add title

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+            title: "DocPal",
             link: [
                 { ref: "manifest", href: '/manifest.json' },
             ]
