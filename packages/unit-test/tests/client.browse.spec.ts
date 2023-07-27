22fe1e6eae9552138b9d4f4c2e945a4e79424347
Sean Tsang <seantsang83@gmail.com>
2023-07-27 09:35:19 +0800

    update same folder test

--- packages/unit-test/tests/client.browse.spec.ts
+++ packages/unit-test/tests/client.browse.spec.ts
@@ -24,46 +24,6 @@ test.describe('create folder', () => {
     
   })
   
-  test('from right click', async ({page}) => {
-    const newFolderName = 'testFolder_right_click_' + Date.now();
-    const secondFolderName = 'testFolder_' + Date.now() + 1;
-    
-    await page.goto('/browse');
-    const firstItemInTable = await page.locator('.nameContainer > .dropzone').first()
-    const title = await firstItemInTable.innerText();
-    await firstItemInTable.dblclick();
-    await expect(page.locator('.breadContainer').filter({ hasText: title })).toHaveCount(1);
-
-    // create folder from top menu
-    await page.locator('#newActionButton').click();
-    await page.getByRole('menuitem', { name: 'New Folder' }).click();
-    await page.getByLabel('File Name').fill(newFolderName);
-    await page.getByRole('button', { name: 'Submit' }).click();
-    await page.getByRole('cell', { name: newFolderName }).click({
-      button: 'right'
-    });
-
-    await page.getByRole('menuitem', { name: 'New Folder' }).click();
-    await page.getByLabel('File Name').fill(secondFolderName);
-    await page.getByRole('button', { name: 'Submit' }).click();
-    
-    await page.getByRole('cell', { name: newFolderName }).dblclick();
-    // delete folder
-    await page.getByRole('cell', { name: secondFolderName }).click({
-      button: 'right'
-    });
-    await page.getByRole('menuitem', { name: 'Delete' }).click();
-    await page.getByRole('button', { name: 'OK' }).click();
-
-    await page.getByText('Account Documents').click();
-    // delete folder
-    await page.getByRole('cell', { name: newFolderName }).click({
-      button: 'right'
-    });
-    await page.getByRole('menuitem', { name: 'Delete' }).click();
-    await page.getByRole('button', { name: 'OK' }).click();
-    
-  })
   
   test('with same name', async ({page}) => {
     // go to sub folder
@@ -86,6 +46,13 @@ test.describe('create folder', () => {
     await page.getByRole('button', { name: 'Submit' }).click();
     
     await expect(page.getByText('The system detected a duplicate file name, please select Rename or Replace for t')).toHaveCount(1);
+    
+    await page.getByLabel('Close this dialog').click(); // close dialog
+    await page.getByRole('cell', { name: newFolderName }).click({
+      button: 'right'
+    });
+    await page.getByRole('menuitem', { name: 'Delete' }).click();
+    await page.getByRole('button', { name: 'OK' }).click();
   })
   
 })
