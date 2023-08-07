9fbc38d0540a64273226421880f07e24d5842e01
Sean Tsang <seantsang83@gmail.com>
2023-08-07 14:08:38 +0800

    fix file doc type

--- packages/office/components/officeAddin/outlook/upload/attachment.vue
+++ packages/office/components/officeAddin/outlook/upload/attachment.vue
@@ -5,7 +5,7 @@ import {useUpload} from "~/compositbles/upload";
 import { base64StringToBlob } from 'blob-util';
 
 const formJson = getJsonApi('office/officeUpload.json')
-const {form, docTypeList, docTypeChange, CheckDuplicate} = useUpload()
+const {form, docTypeList, fileDocTypeList, docTypeChange, CheckDuplicate} = useUpload()
 const FromRendererRef = ref()
 const metaForm = ref();
 
@@ -57,7 +57,7 @@ defineExpose({
         <el-form :model="form" ref="metaForm">
           <el-form-item label="Document Type" required prop="docType">
             <el-select v-model="form.docType" @change="docTypeChange" filterable>
-              <el-option v-for="item in docTypeList" :key="item.name" :label="item.name" :value="item.name"></el-option>
+              <el-option v-for="item in fileDocTypeList" :key="item.name" :label="item.name" :value="item.name"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item v-for="(item, index) in form.displayMeta" :key="item.metaData" :label="$t(item.metaData)" :prop="`metadata.${item.metaData}`" :required="item.isRequire">
