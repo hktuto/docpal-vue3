9fa68f9945915d760934d6285fd36f6271873f58
Oy <OY.Ou@weltronics.com>
2023-09-14 09:52:18 +0800

    test3

--- packages/dp-auth/components/AuthApp.vue
+++ packages/dp-auth/components/AuthApp.vue
@@ -37,7 +37,7 @@ const props = withDefaults(defineProps<{
   showForgetPassword: true
 })
 function handleAuth() {
-  const superAdmin = route.query.
+  const superAdmin = route.query.superAdmin
   
   if(publicPages.includes(route.path)) {
     appStore.setDisplayState('ready') 
