b1ddf6c9636bf3b313d5a8dba81fd05a757b81dd
Sean Tsang <seantsang83@gmail.com>
2023-10-10 18:03:27 +0800

    browse test case

--- testing/tests/client/browse/table.spec.ts
+++ testing/tests/client/browse/table.spec.ts
@@ -2,6 +2,7 @@ import { test } from '@playwright/test';
 import { client, expect } from '../../../utils/client';
 
 test.describe('Browse List', () => {
+
     client('table visibility', async({browsePage}) => {
         const tableTab = await browsePage.page.locator('#tab-table');
         await tableTab.isVisible(); // check if table is visible
@@ -12,12 +13,108 @@ test.describe('Browse List', () => {
         await table.isVisible();
     });
 
-    client('table content', async({browsePage}) => {
-        // get all table rows
-        const allName = await browsePage.page.locator('.nameContainer > .dropzone');
-        // get name inside allName
-        console.log(allName);
+    client('create folder', async({browsePage}) => {
+        browsePage.goToFirstLevel();
+        const name = 'testFolder_' + Date.now();
+        await browsePage.addFolder(name);
+    })
+
+    client('create folder with same name', async({browsePage}) => {
+        browsePage.goToFirstLevel();
+        const name = 'testFolder_' + Date.now();
+        await browsePage.addFolder(name);
+        await browsePage.addFolder(name);
+        await expect(browsePage.page.getByText('The system detected a duplicate file name, please select Rename or Replace for t')).toHaveCount(1);
+    });
+
+    client('filter table by file name', async({browsePage}) => {
+        browsePage.goToFirstLevel();
+        // create two folder
+        const f1 = 'testFolder_' + Date.now();
+        const f2 = 'testFolder2_' + Date.now();
+        await browsePage.addFolder(f1);
+        await browsePage.addFolder(f2);
+        await browsePage.page.getByPlaceholder('Name').fill(f1);
+        // f2 should not be in the table
+        const count = await browsePage.page.locator('.nameContainer > .label').filter({ hasText: f2 }).count();
+        expect(count).toBe(0);
+        // f1 should be in the table
+        const count2 = await browsePage.page.locator('.nameContainer > .label').filter({ hasText: f1 }).count();
+        expect(count2).toBe(1);
+    });
+
+    client('edit folder name on top action', async({browsePage}) => {
+        await browsePage.goToFirstLevel();
+        const name = 'testFolder_' + Date.now();
+        await browsePage.addFolder(name);
+        const newName = 'testFolder_newname_' + Date.now();
+        await browsePage.page.getByPlaceholder('Name').fill(name);
+        const item = await browsePage.page.locator('.nameContainer > .dropzone').first();
+        item.dblclick();
+        // wait for 2 seconds
+        await browsePage.page.waitForTimeout(2000);
+
+        await expect(browsePage.page.locator('.breadContainer').filter({ hasText: name })).toHaveCount(1);
+        // wait for url include name
+
+        await browsePage.page.locator('#editActionButton span').click();
+        await browsePage.page.getByLabel('name', { exact: true }).fill(newName);
+        await browsePage.page.getByRole('button', { name: 'Save' }).click();
+        browsePage.updateNameInItemToDelete(name, newName);
+        await browsePage.goRoot();
+        await browsePage.goToFirstLevel();
+        await browsePage.page.waitForTimeout(2000);
+        await browsePage.page.getByPlaceholder('Name').fill(newName);
+        const count = await browsePage.page.locator('.nameContainer > .label').filter({ hasText: newName }).count();
+        expect(count).toBe(1);
+    });
+
+    client('edit folder name from right click', async({browsePage}) => {
+        await browsePage.goToFirstLevel();
+        const name = 'testFolder_' + Date.now();
+        const newName = 'testFolder_newname_' + Date.now();
+        await browsePage.addFolder(name);
+        await browsePage.page.waitForTimeout(2000);
+        await browsePage.page.getByPlaceholder('Name').fill(name);
+        await browsePage.page.getByRole('cell', { name }).click({
+            button: 'right'
+          });
+        await browsePage.page.getByRole('menuitem', { name: 'Rename' }).click();
+
+        await browsePage.page.getByLabel('name', { exact: true }).fill(newName);
+        await browsePage.page.getByRole('button', { name: 'Save' }).click();
+        
+        await browsePage.page.waitForTimeout(2000);
+        await browsePage.page.getByPlaceholder('Name').fill(newName);
+        const count = await browsePage.page.locator('.nameContainer > .label').filter({ hasText: newName }).count();
+        expect(count).toBe(1);
+        browsePage.updateNameInItemToDelete(name, newName);
 
     });
+
+    client('delete folder from top action bar', async({browsePage}) => {
+        // create folder
+        await browsePage.goToFirstLevel();
+        const name = 'testFolder_' + Date.now();
+        await browsePage.addFolder(name);
+        // delete folder
+        await browsePage.page.getByPlaceholder('Name').fill(name);
+        const item = await browsePage.page.locator('.nameContainer > .dropzone').first();
+        item.dblclick();
+        await browsePage.page.waitForTimeout(2000);
+        await expect(browsePage.page.locator('.breadContainer').filter({ hasText: name })).toHaveCount(1);
+        await browsePage.page.locator('#deleteActionButton').getByRole('img').click();
+        await browsePage.page.getByRole('button', { name: 'OK' }).click();
+
+        await browsePage.goRoot();
+        await browsePage.goToFirstLevel();
+        
+        await browsePage.page.waitForTimeout(2000);
+        await browsePage.page.getByPlaceholder('Name').fill(name);
+        const count = await browsePage.page.locator('.nameContainer > .label').filter({ hasText: name }).count();
+        expect(count).toBe(0);
+    });
+
+
 
 });
--- testing/utils/client/browse.ts
+++ testing/utils/client/browse.ts
@@ -21,17 +21,24 @@ export class BrowsePage {
         await this.page.getByLabel('File Name').fill(name);
         await this.page.getByRole('button', { name: 'Submit' }).click();
         await this.addItemToDelete(name)
+        await expect(this.page.getByRole('cell', { name })).toHaveCount(1);
     }
 
     async addItemToDelete(name:string) {
         const url = await this.page.url();
         const urlObj = new URL(url);
+        // check name is not in the list
+        const exist = await this.itemToDelete.find(item => item.name === name);
+        if(exist) return;
         this.itemToDelete.push({
             name: name,
             parentPath: urlObj.searchParams.get('path') as string
         });
     }
 
+    async goRoot() {
+        await this.page.locator('.breadItem').first().click();
+    }
     async goToFirstLevel() {
         const firstItemInTable = await this.page.locator('.nameContainer > .dropzone').first()
         const title = await firstItemInTable.innerText();
@@ -39,17 +46,29 @@ export class BrowsePage {
         await expect(this.page.locator('.breadContainer').filter({ hasText: title })).toHaveCount(1);
     }
 
+    async updateNameInItemToDelete(oldName: string, newName: string) {
+        const item = this.itemToDelete.find(item => item.name === oldName);
+        if(item) {
+            item.name = newName;
+        }
+    };
+
     async deleteItem(item: Item) {
         if(!item.parentPath) {
             return;
         }
-        await this.page.goto(`/browse?path=${item.parentPath}`);
+        await this.page.goto(clientURL + `/browse?path=${item.parentPath}`);
         await this.page.getByPlaceholder('Name').fill(item.name);
-        await this.page.getByRole('cell', { name: item.name }).click({
-            button: 'right'
-        });
-        await this.page.getByRole('menuitem', { name: 'Delete' }).click();
-        await this.page.getByRole('button', { name: 'OK' }).click();
+        const count = await this.page.locator('.nameContainer > .label').filter({ hasText: item.name }).count();
+        console.log('count', count)
+        if(count === 1) {
+            // if item is in the table, delete it
+            await this.page.getByRole('cell', { name: item.name }).click({
+                button: 'right'
+            });
+            await this.page.getByRole('menuitem', { name: 'Delete' }).click();
+            await this.page.getByRole('button', { name: 'OK' }).click();
+        }
     }
 
     async removeAll() {
