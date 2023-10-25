f234a6d69ce5889c4c3ca33f7a2aea37f36976f5
Oy <OY.Ou@weltronics.com>
2023-10-25 08:51:28 +0800

    test proxy

--- packages/dp-api/src/endpoint/admin-dashboard.ts
+++ packages/dp-api/src/endpoint/admin-dashboard.ts
@@ -5,102 +5,77 @@ import {
     Response
 } from '../model';
 export const GetDashboardPageApi = async(params: pageParams):Promise<paginationData> => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
-    console.log("externalEndpoint", externalEndpoint.value.dashboard)
     const res = await api.post<Response<paginationData>>('/docpal/user/dashboard/page', params, {
-        // check host name is localhost or not, if no change base url to process.env.DASHBOARD_PROXY
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
-        // baseURL:  '/dashboard'
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 
 export const GetDashboardApi = async(id: string) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.get(`/docpal/user/dashboard/${id}`, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 export const DeleteDashboardApi = async(id: string) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.delete(`/docpal/user/dashboard/${id}`, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 
 export const CreateDashboardApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/user/dashboard', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 export const UpdateDashboardApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.put('/docpal/user/dashboard', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 
 // size
 export const GetDocTypeSizeApi = async(params: any = {}) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/dashboard/DocumentTypeOfSizeByRange', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 export const GetDocTypeSizeTrendApi = async(params: any= {}) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/dashboard/DocumentTypeOfSizeByMonthlyRangeCumulation', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 // count
 export const GetDocTypeCountApi = async(params: any = {}) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/dashboard/DocumentTypeOfCountByRange', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 // co-count count
 export const GetCoCountCountApi = async(primaryType: string, creator?: string) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     if(creator) return await GeCoCountCountSingleUserApi({
         primaryType,
         creator
     })
     const res = await api.post('/docpal/dashboard/NewFilesOfUsersCountByDTypeBymonthlyCumulation', {primaryType}, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 export const GeCoCountCountSingleUserApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/dashboard/NewFilesOfSpecifyUserCountByDTypeBymonthlyCumulation', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 // co-count size
 export const GetCoCountSizeApi = async(primaryType: string, creator?: string) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     if (creator) {
         return await GeCoCountSizeSingleUserApi({
             primaryType,
@@ -108,54 +83,42 @@ export const GetCoCountSizeApi = async(primaryType: string, creator?: string) =>
         })
     }
     const res = await api.post('/docpal/dashboard/NewFilesOfUsersSizeByDTypeBymonthlyCumulation', {primaryType}, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 export const GeCoCountSizeSingleUserApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/dashboard/NewFilesOfSpecifyUserSizeByDTypeBymonthlyCumulation', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 // co-count meta
 export const GetCoCountMetaApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     if (params.creator) return await GeCoCountMetaSingleUserApi(params)
     const res = await api.post('/docpal/dashboard/NewFilesOfUsersMetaByDTypeByRange', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 export const GeCoCountMetaSingleUserApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/dashboard/NewFilesOfSpecifyUserMetaByDTypeByRange', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 
 // co-count meta filter
 export const GetCoCountMetaFilterApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     if (params.creator) return await GeCoCountMetaFilterSingleUserApi(params)
     const res = await api.post('/docpal/dashboard/NewFilesOfUserByDTypeByRangeFilterMatedata', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
 export const GeCoCountMetaFilterSingleUserApi = async(params: any) => {
-    // @ts-ignore
-    const {externalEndpoint} = useSetting()
     const res = await api.post('/docpal/dashboard/NewFilesOfSpecifyUserByDTypeByRangeFilterMatedata', params, {
-        baseURL: location.hostname === 'localhost' ? '/dashboard' : externalEndpoint.value.dashboard
+        baseURL: '/dashboard'
     }).then(res => res.data.data)
     return res
 }
-
-
--- packages/dp-stores/nuxt.config.ts
+++ packages/dp-stores/nuxt.config.ts
@@ -9,13 +9,45 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
     ],
     // @ts-ignore
+    axios: {
+        // 开启代理 (如果需要判断线上线下环境，可以通过 process.env.NODE_ENV !== 'production' 来判断)
+        proxy: true,
+        // 给请求 url 加个前缀 /api，如果这项不配置，则需要手动添加到请求链接前面
+        // 如果是多个代理的时候，则不需要配置，走手动添加代理前缀
+        prefix: '/api',
+        // 跨域请求时是否需要使用凭证
+        credentials: true
+    },
+    proxy: {
+        //当代理多个时，只需添加如下格式的数组对象即可
+        '/api': {     //  key(路由前缀)：value(target)(代理地址)
+            // 目标接口域   
+            target: process.env.PROXY,
+            // 全局配置是否跨域
+            changeOrigin: true,
+            pathRewrite: {
+                // 单个配置是否跨域
+                // changeOrigin: true
+                // 把 '/api' 替换成 '/'，具体需要替换为 '' 还是 '/' 看自己习惯
+                '^/api': '/api'
+            }
+        },
+        '/dashboard':{
+            target: process.env.DASHBOARD_PROXY,
+            changeOrigin: true,
+            pathRewrite: {
+                '^/dashboard': '/'
+            }
+        }
+    },
+    // @ts-ignore
     pinia: {
         autoImports: [
           // automatically imports `defineStore`
           'defineStore', // import { defineStore } from 'pinia'
           ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
         ],
-      },
+    },
     nitro:{
         devProxy:{
             '/api':{
@@ -28,11 +60,6 @@ export default defineNuxtConfig({
                 changeOrigin: true,
                 prependPath: true,
             },
-        },
-        routeRules: {
-            '/dashboard': {
-                proxy: dashboardProxy
-            }
         }
     }
 })
