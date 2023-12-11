a5cc2bbac00a3ffb3681f770e45bde7824fb2d2e
Sean Tsang <seantsang83@gmail.com>
2023-12-11 10:50:11 +0800

    fix build 2

--- clients/chevalier/nuxt.config.ts
+++ clients/chevalier/nuxt.config.ts
@@ -3,11 +3,34 @@ import { customLayer } from "./packageManage";
 
 const config = defineNuxtConfig({
   ssr:false,
-
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+      title: "DocPal",
+      meta:[
+        {
+          name:"apple-mobile-web-app-capable", content:"yes"
+        },
+        {
+          name:"apple-mobile-web-app-status-bar-style", content:"white"
+        },{
+          name:"apple-mobile-web-app-title", content:"DocPal"
+        },
+      ],
+      link: [
+        { ref: "manifest", href: '/site.webmanifest' },
+      ]
+    }
+  },
+  devtools: {
+    enabled: true,
+  },
   // color mode setting
   extends: [
-    "../../apps/client"
+    ...customLayer()
   ],
+  ...playgroundConfig({}),
 })
 
 
