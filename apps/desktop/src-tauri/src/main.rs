ae364bc83e7d2f59d3c95804739248e88d9af808
DESKTOP-KNC0P1R\sean <seantsang83@gmail.com>
2023-12-24 08:37:24 +0800

    window setup

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,32 +1,15 @@
-
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri_plugin_autostart::MacosLauncher;
-use tauri::Manager;
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
 fn main() {
-    tauri_plugin_deep_link::prepare("com.wclsolution.docpal");
-    
-  tauri::Builder::default()
-    .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec!["--flag1", "--flag2"]) /* arbitrary number of args to pass to your app */))
-    .setup(|app|  {
-        let handle = app.handle();
-            println!("Setup start");
-          tauri_plugin_deep_link::register(
-            "docpal",
-            move |request| {
-              dbg!(&request);
-              println!("{}", &request);
-              handle.emit_all("scheme-request-received", request).unwrap();
-            },
-          ).unwrap(/* If listening to the scheme is optional for your app, you don't want to unwrap here. */);
-          
-          #[cfg(not(target_os = "macos"))] // on macos the plugin handles this (macos doesn't use cli args for the url)
-            if let Some(url) = std::env::args().nth(1) {
-              app.emit_all("scheme-request-received", url).unwrap();
-            }
-        Ok({})
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
