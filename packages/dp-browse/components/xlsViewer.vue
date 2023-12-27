b99525af0f0cbacea87401d718d586561d7de9a5
Sean Tsang <seantsang83@gmail.com>
2023-12-27 14:43:55 +0800

    tem

--- packages/dp-browse/components/xlsViewer.vue
+++ packages/dp-browse/components/xlsViewer.vue
@@ -5,20 +5,26 @@ const props = defineProps<{
     doc: any
 }>()
 const { doc } = toRefs(props)
-const iframeSrc = ref('');
+const formEl = ref();
+const collaboraUrl = ref('');
+const css = ref('');
+const ui = ref('UIMode=Default')
+const token = ref('')
 
 async function displayIframe(){
-    const token = await getOfficeTokenApi(props.doc.id)
-    iframeSrc.value = officeUrl(props.doc.id, token)
+
+    token.value = await getOfficeTokenApi(props.doc.id)
+    collaboraUrl.value = officeUrl(props.doc.id, token)
+    formEl.value.submit
 }
 
 
 const officeUrl = (docId:string, token:string) =>{
     let host = window.location.host.replace('admin.', '');
     if(!host.includes('localhost')){
-        return `https://office.${host}/browser/85ac843/cool.html?WOPISrc=https://office.${host}/wopi/files/${docId}?access_token=${token}`
+        return `https://office.${host}/browser/85ac843/cool.html?WOPISrc=https://office.${host}/wopi/files/${docId}`
     }else{
-        return `https://office.app4.wclsolution.com/browser/85ac843/cool.html?WOPISrc=https://office.app4.wclsolution.com/wopi/files/${docId}?access_token=${token}`
+        return `https://office.app4.wclsolution.com/browser/85ac843/cool.html?WOPISrc=https://office.app4.wclsolution.com/wopi/files/${docId}`
     }
 }
 
@@ -34,7 +40,13 @@ watch(doc, ()=>{
 
 <template>
 <div class="xlsContainer">
-    <iframe ref="xlsxIframe" id="xlsxIframe" class="xlsxIframe" frameborder="0" :src="iframeSrc"></iframe>
+    <form ref="formEl" :action="collaboraUrl" enctype="multipart/form-data" method="post" target="collabora-online-viewer" id="collabora-submit-form">
+        <input name="css_variables" :value="css" type="hidden" id="css-variables"/>
+        <input name="ui_defaults" :value="ui" type="hidden" id="ui-defaults"/>
+        <input name="access_token" :value="token" type="hidden" id="access-token"/>
+        <input type="submit" value="" />
+    </form>
+    <iframe ref="xlsxIframe" id="xlsxIframe" class="xlsxIframe" frameborder="0" name="collabora-online-viewer"></iframe>
 </div>
 </template>
 
