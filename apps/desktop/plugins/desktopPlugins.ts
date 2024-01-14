fb9ed3b7fbf2232e8c8698a0f6e07b37140d0fe4
Sean Tsang <9394wing@gmail.com>
2024-01-14 14:45:24 +0800

    add basic notification support

--- apps/desktop/plugins/desktopPlugins.ts
+++ apps/desktop/plugins/desktopPlugins.ts
@@ -2,13 +2,22 @@
 
 
 export default defineNuxtPlugin(nuxtApp => {
-    // const { headerSlots } = useLayout()
+    const { messageHandlers } = useNotification()
+    
+    function notificationHandler(message:any) {
+        // TODO : handle different message type
+        new window.Notification( "New Message", {
+            icon: "http://localhost:3000/images/logo-white_logo-light.svg",
+            body: "new message body"
+        }).onclick = () => {
+            console.log("notification click")
+        }
+        
+    }
 
-    // headerSlots.value.push({
-    //     name: "tauri",
-    //     order: 0,
-    //     component: TauriHeader
-    // })
-
+    messageHandlers.value.push({
+        name:'nativeNotification',
+        handler:notificationHandler
+    })
 
 })
