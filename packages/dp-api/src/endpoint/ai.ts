9622020c7824ec72af1385f7639818b6f88b565a
oy <OY.Ou@weltronics.com>
2024-01-16 16:11:13 +0800

    [api-change]/ask_ai/ai_chat

--- packages/dp-api/src/endpoint/ai.ts
+++ packages/dp-api/src/endpoint/ai.ts
@@ -4,15 +4,15 @@ import {
     paginationData,
 } from '../model';
 export const AiChatInitApi = async() => {
-    const res = await api.get('/ai/queryAiChatInitInfo', { }).then(res => res.data.data)
+    const res = await api.get('/ai/ask_ai/ai_chat/queryAiChatInitInfo', { }).then(res => res.data.data)
     return res
 }
 export const AddAiCommentApi = async(answerId) => {
-    const res = await api.post('/ai/addAiComment', { answerId }).then(res => res.data.data)
+    const res = await api.post('/ai/ask_ai/ai_chat/addAiComment', { answerId }).then(res => res.data.data)
     return res
 }
 export const AiAaskQuestionApi = async(params) => {
-    const res = await api.post('/ai/askQuestion', params, {
+    const res = await api.post('/ai/ask_ai/ai_chat/askQuestion', params, {
         timeout: 2 * 60000
     }).then(res => res.data.data)
     return res
