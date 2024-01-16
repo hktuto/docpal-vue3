9e1fe192e80ef6cbd61364a0a3272be7d9fe2284
oy <OY.Ou@weltronics.com>
2024-01-16 16:47:55 +0800

    AddAiCommentApi loading

--- packages/dp-ai/components/ai/chatContent/list.vue
+++ packages/dp-ai/components/ai/chatContent/list.vue
@@ -10,7 +10,7 @@
             </div>
             <div v-else-if="item.type === 'explain'"  :key="item.id" class="ai-chat--explain">
                 <div>{{ item.answer }}</div>
-                <el-button @click="handleAddToComment(item.id)">{{ $t('ai.addTocomment') }}</el-button>
+                <el-button :laoding="state.commentLoading" @click="handleAddToComment(item.id)">{{ $t('ai.addTocomment') }}</el-button>
             </div>
             <AiChatContentAiAction v-else-if="item.type === 'loading'" placeholder="ai.generatingAnswer"/>
             <AiChatContentAiAction v-else :placeholder="`ai.${item.type}`"/>
@@ -31,8 +31,16 @@ const props = withDefaults(defineProps<{
     chatRecord: []
 })
 
+const state = reactive<any>({
+    commentLoading: false
+})
+
 async function handleAddToComment(id) {
+    state.commentLoading = true
     await AddAiCommentApi(id)
+    setTimeout(() => {
+        state.commentLoading = false
+    }, 1000);
 }
 </script>
 <style lang="scss" scoped>
