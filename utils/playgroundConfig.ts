6bff5bb7197fb872d82c9d49f80ad6eaa9bd88aa
Sean Tsang <seantsang83@gmail.com>
2024-01-16 18:12:47 +0800

    remove env from runtime config

--- utils/playgroundConfig.ts
+++ utils/playgroundConfig.ts
@@ -8,19 +8,7 @@ const defaultConfig = {
   runtimeConfig:{
       public:{
         env,
-        mode: process.env.NODE_ENV,
-        pdfReaderUrl: 'https://' + process.env.DOCPAL_END_POINT + process.env.pdfReaderUrl,
-        LOCAL_KEY: process.env.LOCAL_KEY || 'client,meta',
-        PROXY: process.env.PROXY || 'https://app4.wclsolution.com/api',
-        DASHBOARD_PROXY: process.env.DASHBOARD_PROXY || 'https://app4.wclsolution.com/public-api/report/v1/api',
-        CLIENT_PROXY: process.env.CLIENT_PROXY || 'https://app4.wclsolution.com/api',
-        ADMIN_PROXY: process.env.ADMIN_PROXY || 'https://admin.app4.wclsolution.com/api',
-        endPoint: process.env.endPoint || 'client',
-        DEFAULT_PATH: process.env.endPoint === 'admin' ? '/acl' : '/browse',
-        OFFICE_END_POINT : process.env.OFFICE_END_POINT || "office.app4.wclsolution.com",
-        UPLOAD_END_POINT : process.env.UPLOAD_END_POINT || "upload.app4.wclsolution.com",
-        DOCPAL_END_POINT : process.env.DOCPAL_END_POINT || "app4.wclsolution.com",
-        ADMIN_END_POINT : process.env.ADMIN_END_POINT || "admin.app4.wclsolution.com",
+
       }
   },
   vite:{
